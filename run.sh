d6cc9a26f8ffba650386f91de5e32179d03a0de2
Chen-Han Hsiao (Stanley) <chenhan.hsiao.tw@gmail.com>
2018-10-11 15:31:34 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+usage(){
+	echo "Usage: $0 docker_image_name"
+	echo "  For example: $0 autoware/autoware:1.7.0-kinetic"
+	echo "  For example: $0 x11apps"
+	exit -1
+}
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | head -n 1 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+[ $# -ne 1 ] && usage
+
+docker run -it \
+  -e DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY=$XAUTH \
+  -v "$XAUTH:$XAUTH" \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+  -v "/etc/localtime:/etc/localtime:ro" \
+  -v "$PWD":/root \
+  --privileged \
+  "$@" \
+  /bin/bash
+
